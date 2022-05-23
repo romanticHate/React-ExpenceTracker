d5ca9ac65030d294cabdefafed5dae4406850a76
romanticHate <44272880+romanticHate@users.noreply.github.com>
2022-05-23 08:40:54 -0600

    Initial code
    Cogito ergo sum

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import "./App.css"
+
+import {Header} from "./component/Header"
+import {Balance} from "./component/Balance"
+import {IncomeExpenses} from "./component/IncomeExpenses"
+import {TransactionList} from "./component/TransactionList"
+import {AddTransaction} from './component/AddTransaction'
+
+import {GlobalProvider} from './context/GlobalState'
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
+    </GlobalProvider>
+  )
+}
+
+export default App
